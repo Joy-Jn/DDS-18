b011d951a3ee78ea0cd75f324825386ba7f26ea2
Rei Delas <joy.oliveira@aluno.senai.br>
2024-08-23 15:17:09 -0300

    Adicionandd Cálculo de Média

--- Aula10JS/script.js
+++ Aula10JS/script.js
@@ -1,5 +1,17 @@
-console.log("Está funcionando")
+/*console.log("Está funcionando")
 
-var array = ["Texto1", "Texto2", "Texto3"]
+var array = ["Texto1", 10, 25, "Oswaldo", "Texto2"]
 
-console.log(array)
+//console.log(array[0])
+
+for(var i = 0; i < 5; i++){
+    console.log(array[i])
+}
+
+for(var index array){
+    console.log(array[index])
+}
+
+for(var index of array){
+    console.log(index)
+}*/
